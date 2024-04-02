dc031a477ce7371e5191be9ff131d429337bf4f9
gusfb <gusfb@PC>
2024-04-02 20:37:44 -0300

    Add project files.

---
+++ src/App/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleCrudExample
+{
+    public class AppDbContext
+    {
+        public class Tarefa
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public bool Concluida { get; set; }
+        }
+
+        public class ApplicationContext : DbContext
+        {
+            public DbSet<Tarefa> Tarefas { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //String de conexão com o banco de dados
+                optionsBuilder.UseSqlite("Data Source=tarefas.db");
+            }
+        }
+    }
+}
---
+++ src/App/Program.cs
@@ -0,0 +1,29 @@
+﻿using static SimpleCrudExample.AppDbContext;
+
+InicializarBancoDeDados();
+
+AdicionarTarefa("Estudar C#");
+ListarTarefas();
+
+static void AdicionarTarefa(string nome)
+{
+    using var db = new ApplicationContext();
+    db.Tarefas.Add(new Tarefa { Nome = nome });
+    db.SaveChanges();
+}
+
+static void ListarTarefas()
+{
+    using var db = new ApplicationContext();
+    var tarefas = db.Tarefas.ToList();
+    foreach (var tarefa in tarefas)
+    {
+        Console.WriteLine($"{tarefa.Id}: {tarefa.Nome} - Concluída: {tarefa.Concluida}");
+    }
+}
+static void InicializarBancoDeDados()
+{
+    using var contexto = new ApplicationContext();
+
+    contexto.Database.EnsureCreated();
+}
